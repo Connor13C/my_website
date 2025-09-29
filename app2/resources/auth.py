31ccc88d33f61d88a309d506f32b0d56e2da40e1
Connor Carey <connorblazecarey@gmail.com>
2025-09-29 15:08:32 -0600

    cleanup

--- app2/resources/auth.py
+++ app2/resources/auth.py
@@ -102,4 +102,4 @@ def safe_str_cmp(a: str, b: str) -> bool:
     if isinstance(b, str):
         b = b.encode("utf-8")  # type: ignore
 
-    return hmac.compare_digest(a, b)
+    return compare_digest(a, b)
