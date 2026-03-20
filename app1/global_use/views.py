340548ac30db376edeb2c6450f1e045cb4a33b80
Connor Carey <connorblazecarey@gmail.com>
2026-03-20 13:14:24 -0600

    removing unneeded since main app moved away from this one

--- app1/global_use/views.py
+++ app1/global_use/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, "home.html")
-
-def resume(request):
-    return render(request, "resume.html")
+def index(request):
+    return render(request, "index.html")
