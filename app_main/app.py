e80dc45703da21456027d0da87d6c8018a66403f
Connor Carey <connorblazecarey@gmail.com>
2026-04-14 12:40:38 -0600

    breaking change in Starlette version so had to fix

--- app_main/app.py
+++ app_main/app.py
@@ -27,10 +27,10 @@ jinja2.env.globals['remove_subdomain'] = remove_subdomain
 
 @app.get("/", response_class=HTMLResponse, name='index')
 async def index(request: Request):
-    return jinja2.TemplateResponse("index.html", {'request': request})
+    return jinja2.TemplateResponse(request=request, name="index.html")
 
 
 @app.get("/resume", response_class=HTMLResponse)
 async def resume(request:Request):
-    return jinja2.TemplateResponse("Carey,Connor-Resume.html", {'request': request})
+    return jinja2.TemplateResponse(request=request, name="Carey,Connor-Resume.html")
 
