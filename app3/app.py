7cb469233fdf1383fa133169e0414fe92ba91851
Connor Carey <connorblazecarey@gmail.com>
2026-04-18 22:27:09 -0600

    add celery in facory pattern

--- app3/app.py
+++ app3/app.py
@@ -1,3 +1,6 @@
+import os
+
+from celery import Celery
 from fastapi import FastAPI
 
 from users import urls
@@ -18,3 +21,23 @@ def create_app() -> FastAPI:
         return {"message": f"Hello {name}"}
 
     return app
+
+def create_celery():
+    """Configures and creates celery application to run background tasks with the same abilities as the flask
+    application but allows asynchronous tasks to be run during in background."""
+    # app = create_app()
+    celery = Celery(
+        'worker',
+        backend=os.environ.get('CELERY_RESULT_BACKEND'),
+        broker=os.environ.get('CELERY_BROKER_URL')
+    )
+    # celery.conf.update(app.config)
+    #
+    # class ContextTask(celery.Task):
+    #     def __call__(self, *args, **kwargs):
+    #         with app.app_context():
+    #             return self.run(*args, **kwargs)
+    #
+    # celery.Task = ContextTask
+    celery.autodiscover_tasks([entry.name for entry in os.scandir() if entry.is_dir()])
+    return celery
